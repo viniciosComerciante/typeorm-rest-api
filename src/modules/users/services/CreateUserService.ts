da499782287ad39d1363912711c326969c9401b8
viniciosComerciante <vinicompif@gmail.com>
2022-07-05 16:52:40 -0300

    feat:sparkles:: users services

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userExists = await usersRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new AppError('There is already one user with this email');
+    }
+
+    const user = await usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
