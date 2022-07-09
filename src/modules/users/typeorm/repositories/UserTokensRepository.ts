094ecb78958d1c5e2efd0e016b337db4dd588ea1
viniciosComerciante <vinicompif@gmail.com>
2022-07-09 14:22:35 -0300

    feat:sparkles:: usertoken entity and repository

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,22 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+class UserTokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+    return userToken;
+  }
+
+  public async generate(user_id: string): Promise<UserToken | undefined> {
+    const user_token = await this.create({ user_id });
+    await this.save(user_token);
+    return user_token;
+  }
+}
+
+export default UserTokensRepository;
