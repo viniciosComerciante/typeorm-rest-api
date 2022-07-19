59b9002489b963e9e174685b7303b63f3219e9f6
viniciosComerciante <vinicompif@gmail.com>
2022-07-18 22:39:37 -0300

    feat :sparkles: orders_product migrations

--- src/shared/typeorm/migrations/1658193408829-CreateOrdersProducts.ts
+++ src/shared/typeorm/migrations/1658193408829-CreateOrdersProducts.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateProducts1656903750471 implements MigrationInterface {
+export class CreateOrdersProducts1658193408829 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'products',
+        name: 'orders_products',
         columns: [
           {
             name: 'id',
@@ -12,10 +12,6 @@ export class CreateProducts1656903750471 implements MigrationInterface {
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
-          },
-          {
-            name: 'name',
-            type: 'varchar',
           },
           {
             name: 'price',
@@ -43,7 +39,6 @@ export class CreateProducts1656903750471 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('products');
+    await queryRunner.dropTable('orders_products');
   }
 }
-
